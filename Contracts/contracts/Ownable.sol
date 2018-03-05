49f154d9b1b11557dd74ca6e2f37588829ab0b21
Svetoslav Petkov <svetoslav.petkov@gmail.com>
2018-03-05 17:01:15 +0200

    Initial DApp commit

---
+++ Contracts/contracts/Ownable.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.20;
+contract Ownable {
+    address public owner;
+
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+}
